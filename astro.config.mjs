0b8d1af2c8b86aae36f5c35b88f208e2da89f35f
Ricardo Chávez <75815255+TheWhiteLizard@users.noreply.github.com>
2024-04-24 09:59:14 -0600

    first commit 1

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import compress from 'astro-compress';
+
+// https://astro.build/config
+export default defineConfig({
+
+  site: 'https://conceptoone.github.io',
+  experimental: {
+    viewTransitions: true,
+  },
+  integrations: [tailwind(), compress()],
+  
+});
