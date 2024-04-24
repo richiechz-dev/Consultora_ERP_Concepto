aca058d2cd3052225c19eccbe203e79dfc335a68
Ricardo Chávez <75815255+TheWhiteLizard@users.noreply.github.com>
2024-04-24 10:17:25 -0600

    first commit main deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import compress from 'astro-compress';
 // https://astro.build/config
 export default defineConfig({
 
-  site: 'https://concepto.github.io',
+  site: 'https://conceptouno.github.io',
   experimental: {
     viewTransitions: true,
   },
